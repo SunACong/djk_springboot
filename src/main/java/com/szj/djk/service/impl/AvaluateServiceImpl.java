1d53a8e9c8402c8c3dcf143454251f871fe9d139
beibaoke <1667568637@qq.com>
2022-10-12 12:10:20 +0800

    jiejue冲突

---
+++ src/main/java/com/szj/djk/service/impl/AvaluateServiceImpl.java
@@ -0,0 +1,16 @@
+package com.szj.djk.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.szj.djk.entity.Avaluate;
+import com.szj.djk.mapper.AvaluateMapper;
+import com.szj.djk.service.AvaluateService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName AvaluateServiceImpl
+ * @Author Administrator
+ * @Create 2022/10/8 0008 下午 17:05
+ */
+@Service
+public class AvaluateServiceImpl extends ServiceImpl<AvaluateMapper, Avaluate> implements AvaluateService {
+}
