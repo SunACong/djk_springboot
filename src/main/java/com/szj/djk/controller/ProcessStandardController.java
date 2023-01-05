81520a37802e3c9ada10473bdbae8f906028c3b1
acongsun <2683885184@qq.com>
2023-01-05 21:00:08 +0800

    新增产品标准查询

--- src/main/java/com/szj/djk/controller/ProcessStandardController.java
+++ src/main/java/com/szj/djk/controller/ProcessStandardController.java
@@ -1,6 +1,11 @@
 package com.szj.djk.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.szj.djk.common.R;
+import com.szj.djk.entity.LmdpThirdConcise;
+import com.szj.djk.entity.ProcessStandard;
 import com.szj.djk.entity.ProcessStandard;
 import com.szj.djk.service.ProcessStandardService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName ProcessStandardController
@@ -21,6 +29,23 @@ public class ProcessStandardController {
 
     @Autowired
     private ProcessStandardService processStandardService;
+
+    @GetMapping("list")
+    public R<List<ProcessStandard>> list(ProcessStandard processStandard){
+        LambdaQueryWrapper<ProcessStandard> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.setEntity(processStandard);
+        List<ProcessStandard> list = processStandardService.list(queryWrapper);
+        return R.success(list);
+    }
+
+    @GetMapping("pageList")
+    public R<Page> pageList(int pageNum, int pageSize, ProcessStandard processStandard){
+        Page<ProcessStandard> pageInfo = new Page<>(pageNum, pageSize);
+        LambdaQueryWrapper<ProcessStandard> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.setEntity(processStandard);
+        Page<ProcessStandard> page = processStandardService.page(pageInfo, queryWrapper);
+        return R.success(page);
+    }
 
     @GetMapping("{id}")
     public R<ProcessStandard> getStandardbyId(@PathVariable("id") int id){
