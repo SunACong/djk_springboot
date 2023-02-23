1cb1bb27b074694431e3a123e7e1ca3ed225095f
卖女孩的小火柴 <1096252673@qq.com>
2023-02-23 19:37:22 +0800

    流程功能更新

---
+++ src/main/java/com/szj/djk/mapper/LmdpCastReelStoreRecordMapper.java
@@ -0,0 +1,20 @@
+package com.szj.djk.mapper;
+
+import com.szj.djk.entity.LmdpCastReelStoreRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author JH-Sparrow
+* @description 针对表【lmdp_cast_reel_store_record(铸轧仓储记录)】的数据库操作Mapper
+* @createDate 2023-02-22 11:35:18
+* @Entity com.szj.djk.entity.LmdpCastReelStoreRecord
+*/
+@Mapper
+public interface LmdpCastReelStoreRecordMapper extends BaseMapper<LmdpCastReelStoreRecord> {
+
+}
+
+
+
+
